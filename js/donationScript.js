2b58cd6868f32d4965145190db52cf193fde857f
Lorenzo <moares.lorenzo@gmail.com>
2024-11-13 12:13:06 +0000

    Donation page finalization and minor css styling

--- js/donationScript.js
+++ js/donationScript.js
@@ -1,33 +1,48 @@
-//Author: Lorenzo Moares Nunez
+//Author: Lorenzo Moares Nunez x23378441
 
 //creating donation array for comment section, including premade messages
-let donation = ["Lorenzo","10","Wow! Great job!","Jean","45","Glad to help!","Jeahyuk","50","Every little bit helps!"];
+let donation = ["Lorenzo","10.00","Wow! Great job!","Jean","45.00","Glad to help!","Jeahyuk","50.00","Every little bit helps!"];
+let currency = "€";
+
+function setCurrency(x)
+{
+	//default currency is EURO. currency changes depending on user selection
+	currency = x;
+	//sets Pledge input box's placeholder as chosen currency
+	document.getElementById("donatePledge").placeholder = currency;
+}
 
 function donoPrint(x,y,z)
 {
 	//prints donation page information into comment section
 	let print = document.getElementById("commentSection");
 	let prepend = print.innerHTML; //allows for new comments to be added at the top of the comment section
-	print.innerHTML = "<b>Name:</b> " + donation[x] + "<br><b>Pledged Amount:</b> €" + donation[y] + "<br><br><b>Comment:</b> " + donation[z] + "<hr>" + prepend;
+	print.innerHTML = "<b>Name:</b> " + donation[x] + "<br><b>Pledged Amount:</b> "+ currency + donation[y] + "<br><br><b>Comment:</b> " + donation[z] + "<hr>" + prepend;
 }
 
 function donoSubmit()
 {
-	//prepends donation array with values from donation page form. takes those elements and adds them to the start of the array, shifting all other elements.
-	donation.unshift(document.getElementById("donateName").value,document.getElementById("donatePledge").value,document.getElementById("donateComment").value);
 	
+	let userName = document.getElementById("donateName").value;
+	//userPledge input is converted from a string to a numeric(float) value, rounded to nearest hundredths decimal place and uses toLocaleString to take the new value and return a string that is a currency sensitive representation of that number. ie: float 10000.00 is converted to 10,000.00, float 1234.567 is converted to 1234.57, etc.
+	let userPledge = parseFloat(document.getElementById("donatePledge").value).toFixed(2).toLocaleString(undefined,{minimumFractionDigits: 2, style: "currency", currency: "GBP"});
+	let userComment = document.getElementById("donateComment").value;
+	
+	//prepends donation array with values from donation page form. takes those elements and adds them to the start of the array, shifting all other elements.
+	donation.unshift(userName,userPledge,userComment);
 	donoPrint(0,1,2);
-}
-
-function premadeDonations()
-{
-	//adds premade messages to comment section on load, prepending values to the start of the donation array
-	for (let i = 0; i < donation.length; i+=3)
+	
+	if (document.getElementById("newsletterOptIn").checked)
 	{
-		//calls for index 0,1,2 of the donation array to be printed, then 3,4,5, etc
-		donoPrint(i,i+1,i+2);
+		document.getElementById("newsletterActive").innerHTML = "Your email: <b>" + document.getElementById("donateEmail").value + "</b><br> has been added to our Newsletter.<br>Thank You.";
+		document.getElementById("newsletterActive").style.backgroundColor = "#F0F5F9";
+	}else
+	{
+		document.getElementById("newsletterActive").innerHTML = "";
+		document.getElementById("newsletterActive").style.backgroundColor = "";
 	}
 }
+
 
 function validate()
 {	
@@ -56,11 +71,17 @@ function validate()
 	}
 }
 
-function init()
+function premadeDonations()
 {
-	premadeDonations();
+	//set currency for premade donations
+	setCurrency(currency);
+	
+	//adds premade messages to comment section on page load
+	for (let i = 0; i < donation.length; i+=3)
+	{
+		//calls for index 0,1,2 of the donation array to be printed, then 3,4,5, etc
+		donoPrint(i,i+1,i+2);
+	}
 }
 
-
-
-document.addEventListener("DOMContentLoaded",init,false);
+document.addEventListener("DOMContentLoaded",premadeDonations,false);
