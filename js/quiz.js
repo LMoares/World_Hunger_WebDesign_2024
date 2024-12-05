8da622de582d76ee42bf4c636e2250e6f696b3f5
Lorenzo <moares.lorenzo@gmail.com>
2024-12-05 18:26:52 +0000

    merge final site files to branch

---
+++ js/quiz.js
@@ -0,0 +1,50 @@
+//Author: Jaehyuk Lim x23271329
+
+
+// validate if users input correctly
+function validate(){
+	let valid = true;
+	let msge = "Incomplete form: ";
+	
+	if(document.getElementById("fname").value === ""){
+		msge += "You need to fill firstname. ";
+		valid = false;
+	}
+	
+	else if(document.getElementById("surname").value === ""){
+		msge += "You need to fill surname. ";
+		valid = false;
+	}
+	
+	else if(document.getElementById("Email").value === ""){
+		msge += "You need to fill Email. ";
+		valid = false;
+	}
+	
+	else if(document.getElementById("10000").checked == false &&
+	document.getElementById("20000").checked == false &&
+	document.getElementById("30000").checked == false){
+		msge += "You must check first question";
+		valid = false;
+	}
+	
+	else if(document.getElementById("1000kg").checked == false &&
+	document.getElementById("2000kg").checked == false &&
+	document.getElementById("3000kg").checked == false){
+		msge += "You must check second question";
+		valid = false;
+	}
+	
+	else if(document.getElementById("v_10000").checked == false &&
+	document.getElementById("v_20000").checked == false &&
+	document.getElementById("v_30000").checked == false){
+		msge += "You must check third question";
+		valid = false;
+	}
+	
+	
+	if(!valid){
+		document.getElementById("details").innerHTML = msge;
+	}
+	return valid;
+}
